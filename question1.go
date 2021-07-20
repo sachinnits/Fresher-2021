8eaa8377e2b8707aa615a34502fe17076fa8fa8f
SACHIN RAI <33811486+sachinnits@users.noreply.github.com>
2021-07-20 17:16:32 +0530

    Question1 with method implementations

--- question1.go
+++ question1.go
@@ -21,41 +21,57 @@ func Initialiser(rows, cols int) *Matrix{
 	}
 	return matrix
 }
-func (ourMatrix Matrix) row_size() int {
+//to find out row size
+func (ourMatrix Matrix) rowSize() int {
 	return ourMatrix.numOfRows
 }
-func (ourMatrix Matrix) column_size() int{
+//to find out column size
+func (ourMatrix Matrix) columnSize() int{
 	return ourMatrix.numOfColumns
 }
-
-func main(){
-	ourMatrix := Initialiser(3,3)
-	fmt.Println(ourMatrix.row_size())
-	fmt.Println(ourMatrix.column_size())
-	for row:=0 ; row < ourMatrix.row_size() ; row++{
-		for col:=0; col < ourMatrix.column_size(); col++{
-			ourMatrix.twoDMatrix[row][col] = row + col
-		}
-	}
-	fmt.Println(ourMatrix)
-	secondMatrix := Initialiser(3,3)
-	for row:=0 ; row < secondMatrix.row_size() ; row++{
-		for col:=0; col < secondMatrix.column_size(); col++{
-			secondMatrix.twoDMatrix[row][col] = row + 2*col
-		}
-	}
-	sumMatrix := Initialiser(3,3)
-	for row:=0 ; row < sumMatrix.row_size() ; row++{
-		for col:=0; col < sumMatrix.column_size(); col++{
+// to set element
+func (ourMatrix *Matrix) setElements(rowIndex, colIndex, Value int){
+	 ourMatrix.twoDMatrix[rowIndex][colIndex] = Value
+}
+// to add 2 matrices
+func (ourMatrix Matrix) addMatrix(secondMatrix , sumMatrix *Matrix) {
+	for row:=0 ; row < sumMatrix.rowSize() ; row++ {
+		for col := 0; col < sumMatrix.columnSize(); col++ {
 			sumMatrix.twoDMatrix[row][col] = ourMatrix.twoDMatrix[row][col] + secondMatrix.twoDMatrix[row][col]
 		}
 	}
-	fmt.Println(sumMatrix)
+}
+//to print matrix structure as json
+func (ourMatrix Matrix) printAsJson(){
 	data,err := json.Marshal(ourMatrix)
 	if err != nil{
 		log.Fatal(err)
 	}
 	fmt.Printf("s\n",data)
+}
+func main(){
+	ourMatrix := Initialiser(3,3)
+	fmt.Println(ourMatrix.rowSize())//a part
+	fmt.Println(ourMatrix.columnSize())//b part
+	for row:=0 ; row < ourMatrix.rowSize() ; row++{
+		for col:=0; col < ourMatrix.columnSize(); col++{
+			ourMatrix.twoDMatrix[row][col] = row + col
+		}
+	}
+	fmt.Println(ourMatrix.twoDMatrix)
+	ourMatrix.setElements(2,2,10)//c part
+	fmt.Println(ourMatrix.twoDMatrix)
+	//initialising the second matrix
+	secondMatrix := Initialiser(3,3)
+	for row:=0 ; row < secondMatrix.rowSize() ; row++{
+		for col:=0; col < secondMatrix.columnSize(); col++{
+			secondMatrix.twoDMatrix[row][col] = row + 2*col
+		}
+	}
+	sumMatrix := Initialiser(3,3)//matrix that will store the sum of 2 matrices
+	ourMatrix.addMatrix(secondMatrix,sumMatrix)//d part
+	fmt.Println(sumMatrix.twoDMatrix)
+	ourMatrix.printAsJson()//e part
 
 
 
